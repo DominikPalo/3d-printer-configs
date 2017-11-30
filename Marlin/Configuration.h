0abde5636cd114107e2b5a247637a2bfd04c7fff
Dominik Palo <dominik@palo.sk>
2017-12-01 00:12:17 +0100

    Calibrate the extruder (cold)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -513,14 +513,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 917 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 3, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 3, 10 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
