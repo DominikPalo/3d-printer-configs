3b40f42407c17d21e022c74d3a9ee222ad8b8d55
Dominik Palo <dominik@palo.sk>
2018-05-07 21:01:49 +0200

    Change the main board: Rambo -> Mini-Rambo

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -119,7 +119,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMBO
+  #define MOTHERBOARD BOARD_MINIRAMBO
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
